9e0642cb40a3a3a7d093087225eae982c26deddf
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-31 03:10:28 +0900

    ♻️ Environment: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.10")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
     implementation("io.ktor:ktor-client-core:1.6.0")
+    implementation("io.ktor:ktor-client-cio:1.6.0")
     implementation("io.ktor:ktor-client-websockets:1.6.0")
     implementation("io.ktor:ktor-client-serialization:1.6.0")
-    implementation("io.ktor:ktor-client-auth:1.6.0")
-    implementation("io.ktor:ktor-client-cio:1.6.0")
     api("org.slf4j:slf4j-api:1.7.30")
 
     testImplementation(kotlin("test"))
