c3b914f6c2fc6017e7ef924cfb0623fabfc4c748
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-31 23:07:03 +0900

    🐎 Refactor: moved http client

--- src/main/kotlin/dev/kotx/diskord/rest/RestClient.kt
+++ src/main/kotlin/dev/kotx/diskord/rest/RestClient.kt
@@ -1,9 +1,29 @@
 package dev.kotx.diskord.rest
 
-import dev.kotx.diskord.DiskordImpl
+import dev.kotx.diskord.*
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 
 class RestClient(
-    val diskord: DiskordImpl
+    private val diskord: DiskordImpl
 ) {
+    private val client = HttpClient {
+        expectSuccess = false
 
+        defaultRequest {
+            header(HttpHeaders.Accept, "application/json")
+            header(HttpHeaders.Authorization, "Bot ${diskord.token}")
+            header(HttpHeaders.UserAgent, "DiscordBot (https://github.com/Kotlin-Chan/diskord, development)")
+        }
+
+        Json {
+            serializer = KotlinxSerializer()
+        }
+
+        install(RateLimit)
+    }
 }
