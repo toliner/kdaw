75a41d73bfe690cf8dd837197a24833f0dc92f23
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-03 00:45:06 +0900

    ✨ New: Utility class to make json easily

---
+++ src/main/kotlin/dev/kotx/diskord/util/JsonBuilder.kt
@@ -0,0 +1,46 @@
+package dev.kotx.diskord.util
+
+import kotlinx.serialization.json.*
+
+class JsonBuilder {
+    private var content = mutableMapOf<String, JsonElement>()
+
+    infix fun String.to(value: String?) {
+        content[this] = JsonPrimitive(value)
+    }
+
+    infix fun String.to(value: Number?) {
+        content[this] = JsonPrimitive(value)
+    }
+
+    infix fun String.to(value: Boolean?) {
+        content[this] = JsonPrimitive(value)
+    }
+
+    infix fun String.to(value: JsonElement) {
+        content[this] = value
+    }
+
+    fun build() = JsonObject(content)
+}
+
+class JsonArrayBuilder {
+    private var contents = mutableListOf<JsonElement>()
+
+    operator fun String?.unaryPlus() {
+        contents.add(JsonPrimitive(this))
+    }
+
+    operator fun Number?.unaryPlus() {
+        contents.add(JsonPrimitive(this))
+    }
+
+    operator fun JsonElement.unaryPlus() {
+        contents.add(this)
+    }
+
+    fun build() = JsonArray(contents)
+}
+
+fun json(block: JsonBuilder.() -> Unit) = JsonBuilder().apply(block).build()
+fun jsonArray(block: JsonArrayBuilder.() -> Unit) = JsonArrayBuilder().apply(block).build()
