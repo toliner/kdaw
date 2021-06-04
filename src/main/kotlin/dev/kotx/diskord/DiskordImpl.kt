72f7a3dc304a5898e8dcb536ccaa711f09b2c9c3
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 05:19:59 +0900

    🔧 Update: add params

--- src/main/kotlin/dev/kotx/diskord/DiskordImpl.kt
+++ src/main/kotlin/dev/kotx/diskord/DiskordImpl.kt
@@ -1,9 +1,11 @@
 package dev.kotx.diskord
 
+import dev.kotx.diskord.event.*
 import dev.kotx.diskord.gateway.*
 import dev.kotx.diskord.rest.*
+import kotlin.reflect.*
 
-class DiskordImpl(token: String) : Diskord(token) {
+class DiskordImpl(token: String, val listeners: MutableMap<KClass<out Event>, KFunction<Unit>>, val intents: Int) : Diskord(token) {
     val restClient = RestClient(this)
     val gatewayClient = GatewayClient(this)
 
