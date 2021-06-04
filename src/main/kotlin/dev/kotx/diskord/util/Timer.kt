217182836ad92bedcbe3938b6d27d887e26d9184
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 03:02:32 +0900

    ✨ New: timer utility

---
+++ src/main/kotlin/dev/kotx/diskord/util/Timer.kt
@@ -0,0 +1,32 @@
+package dev.kotx.diskord.util
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.*
+import kotlin.system.*
+
+fun CoroutineScope.timer(
+    interval: Long,
+    fixedRate: Boolean = true,
+    context: CoroutineContext = EmptyCoroutineContext,
+    action: suspend TimerScope.() -> Unit
+) = launch(context) {
+    val scope = TimerScope()
+
+    while (isActive) {
+        val time = measureTimeMillis { action(scope) }
+
+        if (scope.isCanceled) break
+
+        if (fixedRate) delay(maxOf(0, interval - time)) else delay(interval)
+    }
+}
+
+
+class TimerScope {
+    var isCanceled: Boolean = false
+        private set
+
+    fun cancel() {
+        isCanceled = true
+    }
+}
