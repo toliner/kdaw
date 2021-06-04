b97a4dc973ee841b267d3ad3193bbb11568ccb76
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 03:01:50 +0900

    🔧 Update: add gateway client

--- src/main/kotlin/dev/kotx/diskord/DiskordImpl.kt
+++ src/main/kotlin/dev/kotx/diskord/DiskordImpl.kt
@@ -1,7 +1,13 @@
 package dev.kotx.diskord
 
+import dev.kotx.diskord.gateway.*
 import dev.kotx.diskord.rest.*
 
 class DiskordImpl(token: String) : Diskord(token) {
-    private val restClient = RestClient(this)
+    val restClient = RestClient(this)
+    val gatewayClient = GatewayClient(this)
+
+    override suspend fun test() {
+        gatewayClient.connect()
+    }
 }
