37614de4a7bb0af61a92f0c4f4cf293750e62ab1
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 02:58:38 +0900

    ♻️ Environment: は？

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
     implementation("io.ktor:ktor-client-core:1.6.0")
-    implementation("io.ktor:ktor-client-cio:1.6.0")
+    implementation("io.ktor:ktor-client-okhttp:1.6.0")
     implementation("io.ktor:ktor-client-websockets:1.6.0")
     implementation("io.ktor:ktor-client-serialization:1.6.0")
     api("org.slf4j:slf4j-api:1.7.30")
