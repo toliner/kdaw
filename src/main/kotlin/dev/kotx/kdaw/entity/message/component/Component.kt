cb5f1866d8c835c743c139a62317c1c5bb61f1a8
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 20:55:53 +0900

    🔧 Update: add functions like Component.primary("") {...}

--- src/main/kotlin/dev/kotx/kdaw/entity/message/component/Component.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/component/Component.kt
@@ -11,4 +11,31 @@ interface Component {
     val url: String?
     val disabled: Boolean?
     val components: List<Component>?
+
+    companion object {
+        fun primary(customId: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+            style(ComponentStyle.PRIMARY)
+            customId(customId)
+        }.apply(builder).build()
+
+        fun secondary(customId: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+            style(ComponentStyle.SECONDARY)
+            customId(customId)
+        }.apply(builder).build()
+
+        fun success(customId: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+            style(ComponentStyle.SUCCESS)
+            customId(customId)
+        }.apply(builder).build()
+
+        fun danger(customId: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+            style(ComponentStyle.DANGER)
+            customId(customId)
+        }.apply(builder).build()
+
+        fun link(url: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+            style(ComponentStyle.LINK)
+            customId(url)
+        }.apply(builder).build()
+    }
 }
