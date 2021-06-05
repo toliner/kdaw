d759ad5251700f0ad97406c68bf09fcd8c17d160
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 00:13:08 +0900

    🔧 Update: Kdaw to KdawImpl

--- src/main/kotlin/dev/kotx/kdaw/entity/Entity.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/Entity.kt
@@ -4,7 +4,7 @@ import dev.kotx.kdaw.*
 import java.time.*
 
 interface Entity {
-    val kdaw: Kdaw
+    val kdaw: KdawImpl
     val id: Long
     val timestamp: Instant
         get() = Instant.ofEpochMilli((id ushr 22) + 1420070400000L)
