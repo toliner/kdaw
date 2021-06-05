2051e1dcdf7ff11e065e75fce93a5e2dc8730281
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 17:31:58 +0900

    🔧 Update: add params

--- src/main/kotlin/dev/kotx/kdaw/entity/message/Message.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/Message.kt
@@ -1,6 +1,9 @@
 package dev.kotx.kdaw.entity.message
 
 import dev.kotx.kdaw.entity.*
+import dev.kotx.kdaw.entity.channel.*
+import dev.kotx.kdaw.entity.server.*
+import dev.kotx.kdaw.entity.server.channel.text.*
 import dev.kotx.kdaw.entity.server.member.*
 import dev.kotx.kdaw.entity.user.*
 
@@ -10,4 +13,16 @@ interface Message: Entity {
     val author: User?
 
     val member: Member?
+
+    val server: Server?
+
+    val channel: TextChannel
+
+    val serverChannel: ServerTextChannel?
+
+    val privateChannel: PrivateTextChannel?
+
+    val content: String
+
+
 }
