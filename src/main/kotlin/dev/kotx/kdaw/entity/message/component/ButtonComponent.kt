aacc9533d13e363468c79c92fc7adee22f0d1002
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 00:11:25 +0900

    🔧 Update: add button parser

--- src/main/kotlin/dev/kotx/kdaw/entity/message/component/ButtonComponent.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/component/ButtonComponent.kt
@@ -1,6 +1,8 @@
 package dev.kotx.kdaw.entity.message.component
 
 import dev.kotx.kdaw.entity.server.emoji.*
+import dev.kotx.kdaw.util.*
+import kotlinx.serialization.json.*
 
 class ButtonComponent(
     override val style: ComponentStyle?,
@@ -12,4 +14,14 @@ class ButtonComponent(
 ) : Component {
     override val type: ComponentType = ComponentType.BUTTON
     override val components: List<Component>? = null
+
+    override fun parse(): JsonElement = json {
+        "type" to type.value
+        style?.also { "style" to it.value }
+        label?.also { "label" to it }
+        emoji?.also { "emoji" to it.parse() }
+        customId?.also { "custom_id" to it }
+        url?.also { "url" to it }
+        disabled?.also { "disabled" to it }
+    }
 }
