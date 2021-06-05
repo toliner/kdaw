79e4e84e832b6b2b4ac8e7351ff6e6eb1ba01eab
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 20:54:45 +0900

    🔧 Update: remove attachments, add components

--- src/main/kotlin/dev/kotx/kdaw/entity/message/AbstractMessage.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/AbstractMessage.kt
@@ -1,11 +1,11 @@
 package dev.kotx.kdaw.entity.message
 
-import dev.kotx.kdaw.entity.message.attachment.*
+import dev.kotx.kdaw.entity.message.component.*
 import dev.kotx.kdaw.entity.message.embed.*
 
 interface AbstractMessage {
-    val content: String
-    val embeds: List<Embed>
-    val attachments: List<Attachment>
-    val tts: Boolean
+    val content: String?
+    val embeds: List<Embed>?
+    val components: List<Component>?
+    val tts: Boolean?
 }
