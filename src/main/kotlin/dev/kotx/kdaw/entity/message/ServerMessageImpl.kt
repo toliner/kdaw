0d21cef589fe22e16cceb008f97f6e1941ebe693
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 20:56:41 +0900

    ✨ New: implementation of ServerMessage

---
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/ServerMessageImpl.kt
@@ -0,0 +1,32 @@
+package dev.kotx.kdaw.entity.message
+
+import dev.kotx.kdaw.*
+import dev.kotx.kdaw.entity.channel.*
+import dev.kotx.kdaw.entity.message.attachment.*
+import dev.kotx.kdaw.entity.message.component.*
+import dev.kotx.kdaw.entity.message.embed.*
+import dev.kotx.kdaw.entity.server.*
+import dev.kotx.kdaw.entity.server.channel.text.*
+import dev.kotx.kdaw.entity.server.member.*
+import dev.kotx.kdaw.entity.user.*
+import java.time.*
+
+class ServerMessageImpl(
+    override val kdaw: Kdaw,
+    override val id: Long,
+    override val content: String,
+    override val embeds: List<Embed>,
+    override val components: List<Component>,
+    override val attachments: List<Attachment>,
+    override val tts: Boolean,
+    override val type: MessageType,
+    override val author: User?,
+    override val member: Member?,
+    override val server: Server?,
+    override val channel: TextChannel,
+    override val serverChannel: ServerTextChannel?,
+    override val privateChannel: PrivateTextChannel?,
+    override val editedTimeStamp: Instant?,
+    override val mentionsEveryone: Boolean,
+    override val pinned: Boolean,
+) : ServerMessage
