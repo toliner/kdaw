aeea3b22a630ae0bd68635aa6617c7fcaa4086a5
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 00:11:42 +0900

    🔧 Update: add parserable

--- src/main/kotlin/dev/kotx/kdaw/entity/message/component/Component.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/component/Component.kt
@@ -1,8 +1,9 @@
 package dev.kotx.kdaw.entity.message.component
 
+import dev.kotx.kdaw.entity.*
 import dev.kotx.kdaw.entity.server.emoji.*
 
-interface Component {
+interface Component: Parseable {
     val type: ComponentType
     val style: ComponentStyle?
     val label: String?
