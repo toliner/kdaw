2472bf6ad0fc52244d118590e0e806dd511b2bab
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 17:17:28 +0900

    🔧 Update: add name

--- src/main/kotlin/dev/kotx/kdaw/gateway/GatewayHandler.kt
+++ src/main/kotlin/dev/kotx/kdaw/gateway/GatewayHandler.kt
@@ -3,6 +3,6 @@ package dev.kotx.kdaw.gateway
 import dev.kotx.kdaw.*
 import kotlinx.serialization.json.*
 
-abstract class GatewayHandler {
+abstract class GatewayHandler(val type: String) {
     abstract fun handle(kdaw: KdawImpl, data: JsonObject?)
 }
