fb315f6232635f86eeb16fb12f14c9b13e6b85d2
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 17:21:48 +0900

    ✨ New Base of server channels, categories, voices...

---
+++ src/main/kotlin/dev/kotx/kdaw/entity/server/channel/ServerChannel.kt
@@ -0,0 +1,14 @@
+package dev.kotx.kdaw.entity.server.channel
+
+import dev.kotx.kdaw.entity.*
+import dev.kotx.kdaw.entity.server.*
+import dev.kotx.kdaw.entity.server.permission.*
+
+interface ServerChannel: Nameable, Entity {
+    val server: Server
+
+    val position: Int
+
+    val memberPermissionOverwrites: List<MemberPermissionOverwrite>
+    val rolePermissionOverwrites: List<RolePermissionOverwrite>
+}
