25b97e9ebd811166d5461bea3d077de7b98478c9
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 17:15:38 +0900

    ✨ New: AnnouncementChannel wip

---
+++ src/main/kotlin/dev/kotx/kdaw/entity/server/channel/announcement/AnnouncementChannel.kt
@@ -0,0 +1,17 @@
+package dev.kotx.kdaw.entity.server.channel.announcement
+
+import dev.kotx.kdaw.entity.*
+import dev.kotx.kdaw.entity.channel.*
+import dev.kotx.kdaw.entity.server.channel.*
+import dev.kotx.kdaw.entity.server.channel.category.*
+
+interface AnnouncementChannel : ServerChannel, Mentionable, TextChannel {
+    val topic: String?
+
+    val nsfw: Boolean
+
+    val category: ServerCategory?
+
+    override val mention: String
+        get() = "<#$id>"
+}
