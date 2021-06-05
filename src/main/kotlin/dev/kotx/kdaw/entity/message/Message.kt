1d6a0a56773128443b8ad41732c0a9ecb2b67ee8
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 17:34:35 +0900

    wip

--- src/main/kotlin/dev/kotx/kdaw/entity/message/Message.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/Message.kt
@@ -2,10 +2,13 @@ package dev.kotx.kdaw.entity.message
 
 import dev.kotx.kdaw.entity.*
 import dev.kotx.kdaw.entity.channel.*
+import dev.kotx.kdaw.entity.message.attachment.*
+import dev.kotx.kdaw.entity.message.embed.*
 import dev.kotx.kdaw.entity.server.*
 import dev.kotx.kdaw.entity.server.channel.text.*
 import dev.kotx.kdaw.entity.server.member.*
 import dev.kotx.kdaw.entity.user.*
+import java.time.*
 
 interface Message: Entity {
     val type: MessageType
@@ -24,5 +27,15 @@ interface Message: Entity {
 
     val content: String
 
+    val embeds: List<Embed>
 
+    val attachments: List<Attachment>
+
+    val editedTimeStamp: Instant?
+
+    val mentionsEveryone: Boolean
+
+    val pinned: Boolean
+
+    val tts: Boolean
 }
