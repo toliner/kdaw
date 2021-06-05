9d92770efe9a5488ce90c0af9a3f4563a6b8e221
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-05 20:09:38 +0900

    ✨ New: Server response holder

--- src/main/kotlin/dev/kotx/kdaw/entity/message/ServerMessage.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/ServerMessage.kt
@@ -2,15 +2,13 @@ package dev.kotx.kdaw.entity.message
 
 import dev.kotx.kdaw.entity.*
 import dev.kotx.kdaw.entity.channel.*
-import dev.kotx.kdaw.entity.message.attachment.*
-import dev.kotx.kdaw.entity.message.embed.*
 import dev.kotx.kdaw.entity.server.*
 import dev.kotx.kdaw.entity.server.channel.text.*
 import dev.kotx.kdaw.entity.server.member.*
 import dev.kotx.kdaw.entity.user.*
 import java.time.*
 
-interface Message: Entity {
+interface ServerMessage: Entity, AbstractMessage {
     val type: MessageType
 
     val author: User?
@@ -25,17 +23,9 @@ interface Message: Entity {
 
     val privateChannel: PrivateTextChannel?
 
-    val content: String
-
-    val embeds: List<Embed>
-
-    val attachments: List<Attachment>
-
     val editedTimeStamp: Instant?
 
     val mentionsEveryone: Boolean
 
     val pinned: Boolean
-
-    val tts: Boolean
 }
