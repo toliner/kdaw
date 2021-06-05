760284d1285614acfce79b28833ec612df5bf505
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 00:15:53 +0900

    🔧 Update: add functions

--- src/main/kotlin/dev/kotx/kdaw/entity/channel/TextChannel.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/channel/TextChannel.kt
@@ -2,8 +2,11 @@ package dev.kotx.kdaw.entity.channel
 
 import dev.kotx.kdaw.entity.*
 import dev.kotx.kdaw.entity.message.*
+import dev.kotx.kdaw.entity.message.embed.*
 
-interface TextChannel: Entity {
+interface TextChannel : Entity {
     suspend fun send(text: String)
-    suspend fun send(message: ServerMessage)
+    suspend fun send(message: AbstractMessage)
+    suspend fun send(builder: MessageBuilder.() -> Unit)
+    suspend fun embed(builder: EmbedBuilder.() -> Unit)
 }
