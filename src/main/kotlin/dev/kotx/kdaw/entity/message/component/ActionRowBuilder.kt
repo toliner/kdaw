124f935a05e67205a2c1b181bcdbb6b992eadc5a
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 00:10:27 +0900

    🗑️ Remove: add function only use +Components.xxx

--- src/main/kotlin/dev/kotx/kdaw/entity/message/component/ActionRowBuilder.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/component/ActionRowBuilder.kt
@@ -3,11 +3,6 @@ package dev.kotx.kdaw.entity.message.component
 class ActionRowBuilder {
     private val buttons: MutableList<ButtonComponent> = mutableListOf()
 
-    fun add(component: ButtonComponent): ActionRowBuilder {
-        buttons.add(component)
-        return this
-    }
-
     operator fun ButtonComponent.unaryPlus(): ActionRowBuilder {
         this@ActionRowBuilder.buttons.add(this)
         return this@ActionRowBuilder
