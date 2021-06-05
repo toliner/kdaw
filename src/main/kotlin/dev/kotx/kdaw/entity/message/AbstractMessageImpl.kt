6ccc3b3cd1c21e6b799b927a25e47a39ed1d6ce0
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 00:10:07 +0900

    🔧 Update: add parser

--- src/main/kotlin/dev/kotx/kdaw/entity/message/AbstractMessageImpl.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/AbstractMessageImpl.kt
@@ -2,10 +2,23 @@ package dev.kotx.kdaw.entity.message
 
 import dev.kotx.kdaw.entity.message.component.*
 import dev.kotx.kdaw.entity.message.embed.*
+import dev.kotx.kdaw.util.*
+import kotlinx.serialization.json.*
 
 class AbstractMessageImpl(
     override val content: String?,
-    override val embeds: List<Embed>?,
-    override val components: List<Component>?,
     override val tts: Boolean?,
-) : AbstractMessage
+    override val embed: Embed?,
+    override val components: List<Component>?,
+) : AbstractMessage {
+    override fun parse(): JsonElement = json {
+        content?.also { "content" to it }
+        tts?.also { "tts" to it }
+        embed?.also { "embed" to it.parse() }
+        components?.also { "components" to jsonArray {
+            it.forEach {
+                +it.parse()
+            }
+        } }
+    }
+}
