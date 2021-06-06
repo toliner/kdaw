8ba469fce0b62a48a63085dcfa1306b487c05350
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 23:46:26 +0900

    🔧 Update: add json parser

--- src/main/kotlin/dev/kotx/kdaw/entity/server/permission/PermissionOverwrite.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/server/permission/PermissionOverwrite.kt
@@ -1,12 +1,22 @@
 package dev.kotx.kdaw.entity.server.permission
 
 import dev.kotx.kdaw.entity.*
+import dev.kotx.kdaw.entity.server.role.*
+import dev.kotx.kdaw.util.*
 
 abstract class PermissionOverwrite(
     val entity: Permissionable,
     val allow: List<Permission>,
     val deny: List<Permission>,
 ) {
+
+    fun parse() = json {
+        "id" to if (entity is Role) 0 else 1
+        "type" to 0
+        "allow" to Permission.calculate(allow)
+        "deny" to Permission.calculate(deny)
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is PermissionOverwrite) return false
