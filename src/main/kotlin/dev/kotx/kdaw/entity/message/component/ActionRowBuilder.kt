a1fc3a214e2f4cc634a4849abd7dab5bbbe04336
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 23:44:31 +0900

    🐎 Refactor: moved button builder from Component interface

--- src/main/kotlin/dev/kotx/kdaw/entity/message/component/ActionRowBuilder.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/message/component/ActionRowBuilder.kt
@@ -3,10 +3,30 @@ package dev.kotx.kdaw.entity.message.component
 class ActionRowBuilder {
     private val buttons: MutableList<ButtonComponent> = mutableListOf()
 
-    operator fun ButtonComponent.unaryPlus(): ActionRowBuilder {
-        this@ActionRowBuilder.buttons.add(this)
-        return this@ActionRowBuilder
-    }
+    fun primary(customId: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+        style(ComponentStyle.PRIMARY)
+        customId(customId)
+    }.apply(builder).build()
+
+    fun secondary(customId: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+        style(ComponentStyle.SECONDARY)
+        customId(customId)
+    }.apply(builder).build()
+
+    fun success(customId: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+        style(ComponentStyle.SUCCESS)
+        customId(customId)
+    }.apply(builder).build()
+
+    fun danger(customId: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+        style(ComponentStyle.DANGER)
+        customId(customId)
+    }.apply(builder).build()
+
+    fun link(url: String, builder: ButtonComponentBuilder.() -> Unit) = ButtonComponentBuilder().apply {
+        style(ComponentStyle.LINK)
+        customId(url)
+    }.apply(builder).build()
 
     fun build(): ActionRowComponent = ActionRowComponent(buttons)
 }
