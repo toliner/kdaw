581afba344c073cf78d8dd66a7dcb8134e358049
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-06 23:45:52 +0900

    うぃぷ！w

--- src/main/kotlin/dev/kotx/kdaw/entity/server/channel/announcement/NewsChannelImpl.kt
+++ src/main/kotlin/dev/kotx/kdaw/entity/server/channel/announcement/NewsChannelImpl.kt
@@ -7,8 +7,9 @@ import dev.kotx.kdaw.entity.server.*
 import dev.kotx.kdaw.entity.server.channel.category.*
 import dev.kotx.kdaw.entity.server.permission.*
 import dev.kotx.kdaw.rest.*
+import kotlinx.serialization.json.*
 
-class AnnouncementChannelImpl(
+class NewsChannelImpl(
     override val kdaw: KdawImpl,
     override val id: Long,
     override val name: String,
@@ -17,9 +18,9 @@ class AnnouncementChannelImpl(
     override val memberPermissionOverwrites: List<MemberPermissionOverwrite>,
     override val rolePermissionOverwrites: List<RolePermissionOverwrite>,
     override val topic: String?,
-    override val nsfw: Boolean,
+    override val isNsfw: Boolean,
     override val category: ServerCategory?
-) : AnnouncementChannel {
+) : NewsChannel {
     override suspend fun send(message: AbstractMessage) {
         val data = message.parse()
         kdaw.restClient.request(EndPoint.Channel.Message.Create(id), data)
@@ -43,5 +44,22 @@ class AnnouncementChannelImpl(
 
     override suspend fun crosspostMessage(messageId: Long) {
         kdaw.restClient.request(EndPoint.Channel.Message.Crosspost(id, messageId))
+    }
+
+    override suspend fun edit(editor: NewsChannelEditor.() -> Unit) {
+        val result = NewsChannelEditor(this).apply(editor)
+        kdaw.restClient.request(EndPoint.Channel.Modify(id)) {
+            "name" to result.name
+            "type" to if (result.disableNewsChannel) 0 else 5
+            "position" to result.position
+            "topic" to result.topic
+            "nsfw" to result.isNsfw
+            "permission_overwrites" to (result.memberPermissionOverwrites + result.rolePermissionOverwrites).map { it.parse() }
+            "parent_id" to result.category?.id
+        }
+    }
+
+    override fun update(data: JsonObject) {
+
     }
 }
